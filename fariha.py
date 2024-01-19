4f3306a7bbbde99def8b56aeea539238585a0447
Fariha Ahmad <s1140710@student.hsleiden.nl>
2024-01-19 15:47:01 +0100

    Pic

--- fariha.py
+++ fariha.py
@@ -12,23 +12,18 @@ blauw_pin = 27  # Tweede LED
 groen_pin = 18
 gele_pin = 21
 
-try:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(rood_pin,GPIO.OUT)
-    GPIO.setup(blauw_pin,GPIO.OUT)
-    GPIO.setup(groen_pin,GPIO.OUT)
-    GPIO.setup(gele_pin,GPIO.OUT)
 
-    GPIO.output(rood_pin, GPIO.HIGH)
-    GPIO.output(blauw_pin, GPIO.HIGH)
-    GPIO.output(gele_pin, GPIO.HIGH)
-    GPIO.output(groen_pin, GPIO.HIGH)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(rood_pin,GPIO.OUT)
+GPIO.setup(blauw_pin,GPIO.OUT)
+GPIO.setup(groen_pin,GPIO.OUT)
+GPIO.setup(gele_pin,GPIO.OUT)
+
+GPIO.output(rood_pin, GPIO.HIGH)
+GPIO.output(blauw_pin, GPIO.HIGH)
+GPIO.output(gele_pin, GPIO.HIGH)
+GPIO.output(groen_pin, GPIO.HIGH)
 
 
-except KeyboardInterrupt:
-    print("Cleaning up GPIO")
-    GPIO.cleanup()
 
-finally:
-    GPIO.cleanup()
 
